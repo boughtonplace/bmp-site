840b9a3970f5a1fea89de04d0b6fec69861f3ea5
Tim Kendrick <timkendrick@gmail.com>
2018-02-08 16:01:23 +0000

    Update meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,18 @@ module.exports = {
   siteMetadata: {
     title: 'Boughton Monchelsea Place',
     subtitle: 'Maidstone, Kent ME17 4BU',
-    description: 'Boughton Monchelsea Place is an exquisite venue which may be hired throughout the year.',
-    keywords: ['location', 'shoots', 'photo', 'stately', 'home', 'elizabethan', 'manor', 'corporate'],
+    description:
+      'Boughton Monchelsea Place is an exquisite venue which may be hired throughout the year for location shoots and corporate dining for up to 50. We also offer garden visits from April to September.',
+    keywords: [
+      'location',
+      'shoots',
+      'photo',
+      'stately',
+      'home',
+      'elizabethan',
+      'manor',
+      'corporate',
+    ],
   },
   plugins: [
     {
